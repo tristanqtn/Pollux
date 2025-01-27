0743c4c04023cff1b5b4539c2a2c6ce17d81a6cc
Luxlag <lucie.lagarrigue@edu.ece.fr>
2025-01-27 15:05:07 +0100

    feat : add explanations in report generation

--- pollux/wrapper/report/report.py
+++ pollux/wrapper/report/report.py
@@ -149,3 +149,93 @@ def generate_md_report(file_list, delta_list, output_file):
         report_file.write("\n".join(report_content))
 
     print(f"Report generated: {output_file}")
+
+
+def add_explanations_to_pollux(file_name, report_content):
+    
+    if file_name == "antivirusCheck":
+        report_content.append("This script aims to provide information about the antivirus solution installed on the machine, if it is active, and if some scripts were run recently.\n")
+    
+    elif file_name == "envvarCheck":
+        report_content.append("This script aims to detect abnormal environment variables. These could be clues for malicious activites.\n")
+    
+    elif file_name == "filesystemCheck":
+        report_content.append("This script aims to verify file system permissions and sensitive locations or content. Sensitive files or directories with too much permissions are vulnerable, or could have been modified by malicious actors.\n")
+        
+    elif file_name == "firewallCheck":
+        report_content.append("This script aims to verify firewall configurations, rules, trusted connections and network. A firewall not monitored is potentially vulnerable, as it could have rules that are not up to date. \n")
+        
+    elif file_name == "passwordCheck":
+        report_content.append("This script aims to verify the systems' password policy.\n")
+        
+    elif file_name == "plannedtaskCheck":
+        report_content.append("This script aims to check scheduled tasks running on the system, to detect abnormal ones.\n")
+        
+    elif file_name == "portCheck":
+        report_content.append("This script aims to list open ports and associated service. The goal is to detect unlegitimate open ports.\n")
+        
+    elif file_name == "sessionCheck":
+        report_content.append("This script aims to list verify user sessions and associated privileges. This helps avoid overpriviledged users and privilege escalation. \n")
+        
+    elif file_name == "updateCheck":
+        report_content.append("This script aims to check OS version and packages. It allows to see if the system is up to date, to prevent outdated system issues.\n")
+        
+    """
+    TEMPLATE : 
+    
+    elif file_name == "<>Check":
+        report_content.append("This script aims to ...\n")
+        
+    """   
+
+def add_remediations_to_pollux(file_name, report_content):
+    
+    report_content.append("Good Practices : \n")
+    
+    if file_name == "antivirusCheck":
+        report_content.append("Keep your antivirus up to date, and verify it is running on the system. \n")
+        report_content.append("Use automated scannings. \n")
+        
+        
+    elif file_name == "envvarCheck":
+        report_content.append("Verify regulary your environment variables. Use consistent naming to avoid false positives and keep track of all legitimate variables. \n")
+        report_content.append("If possible, use a tool to monitor environment variables and detect abnormal ones. \n")
+        
+    elif file_name == "filesystemCheck":
+        report_content.append("Implement zero-trust, and give minimal access to sensitive files. \n")
+        report_content.append("If possible, use a file integrity monitoring system to detect unauthorized changes. \n")
+        
+    elif file_name == "firewallCheck":
+        report_content.append(" Implement default deny approach, where all traffic is blocked by default and only necessary services are explicitly allowed. This minimizes exposure to unnecessary threats. \n")
+        report_content.append("Implement principle of least privilege to ensure that access is onlu granted to user or devices that truly require it. \n")
+    elif file_name == "passwordCheck":
+        report_content.append("If not done yet, implement a strong password policy.\n")
+        report_content.append("For users with a password policy, use a password manager to generate strong passwords.\n")
+        report_content.append("As recommended by the NIST, a good password policy should focus more on using long, unique sentences and combine unusual words or unexpected concepts than on a specific length.\n")
+        report_content.append("Plus, password should have at least one special character.\n")
+
+    elif file_name == "plannedtaskCheck":
+        report_content.append("Use consistent naming to easily detect abnormal cron jobs. \n")
+        report_content.append("Check regularly the security of the task by looking at the execution time, the user, and the command. \n")
+        report_content.append("Planned tasks linked to sensitive data must not be readable or writable by non-priviledged users. \n")
+        
+    elif file_name == "portCheck":
+        report_content.append("Expose as few ports as possible, to minimize the attack surface.\n")
+        report_content.append("Monitor opened port to detect unlegitimate ones, and close those who are no longer in use.\n")
+        
+        
+    elif file_name == "sessionCheck":
+        report_content.append("Implement role-based access to simplify users rights management.\n")
+        report_content.append("Always give least privileges to users.\n")
+        
+    elif file_name == "updateCheck":
+        report_content.append("If possible, activate automatic updates. If not check regularly for it, especially when major vulnerabilites are found on the OS.\n")
+        report_content.append("If possible, use a tool to monitor OS and packages versions.\n")
+        
+    """
+    TEMPLATE : 
+    
+    elif file_name == "<>Check":
+        report_content.append("<Remediations>\n")
+        
+    """   
